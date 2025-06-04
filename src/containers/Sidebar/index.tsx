c887b2d3a9784d3a166bfe6b4b7644e690dd9699
arnor <arnorsantosnunes01@gmail.com>
2025-06-04 11:44:01 -0300

    criando a estrutura e a seção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/ArnorSantosNunes.png" />
+    <Titulo fontSize={20}>Arnor Santos Nunes</Titulo>
+  </aside>
+)
+
+export default Sidebar
